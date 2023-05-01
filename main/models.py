6c564064b107d00c82b0aa458c9c731fc36422ca
shimseohyun <shim020924@gmail.com>
2023-05-02 02:06:07 +0900

    feat:수정

--- main/models.py
+++ main/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
+class Post(models.Model):
+    title = models.CharField(max_length= 200)
+    writer = models.CharField(max_length = 100)
+    pub_date = models.DateTimeField()
+    feel = models.CharField(max_length= 30,null=True)
+    weather = models.CharField(max_length= 30,null=True)
+    body = models.TextField()
+
+    def __str__(self):
+        return self.title
+    
+    def summary (self):
+        return self.body[:20]    
 # Create your models here.
--- main/views.py
+++ main/views.py
@@ -1,9 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Post
+from django.utils import timezone
 
 # Create your views here.
 
+def detail(request, id):
+    post = get_object_or_404(Post, pk = id)
+    return render(request, 'main/detail.html', {'post':post})
+
+def create(request):
+    new_post = Post()
+    new_post.title = request.POST['title'] 
+    new_post.writer = request.POST ['writer']
+    new_post.weather = request.POST.get('weather', False)
+    new_post.feel = request.POST.get('feel', False)
+    new_post.pub_date = timezone. now () 
+    new_post.body = request. POST ['body']
+
+    new_post.save ()
+    return redirect('detail', new_post.id)
+
 def mainpage(request):
-    return render(request, 'main/mainpage.html')
+    posts = Post.objects.all ()
+    return render(request, 'main/mainpage.html', {'posts':posts})
 
 def secondpage(request):
     return render(request, 'main/secondpage.html')
+
+def new(request):
+    return render (request, 'main/new.html')
